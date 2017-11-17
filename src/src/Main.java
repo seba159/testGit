300418edff8beabb5a62188595bdbfdba7a34c41
seba <seba159>
2017-11-18 00:05:31 +0100

    e

--- src/src/Main.java
+++ src/src/Main.java
@@ -10,5 +10,13 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
