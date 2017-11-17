0311704843be9992483dd9e35dc8652db124b578
seba <seba159>
2017-11-18 00:07:40 +0100

    buu

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,18 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("buuuu");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("buuuu buuuu!");
         System.out.println("Hello World!");
+        System.out.println("buuuu World!");
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("Hello buuuu!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
